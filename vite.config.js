8153e95e972df77f1dd6561c333da21ad7c94a44
Eris <hi@eris.cafe>
2025-12-09 16:49:19 -0700

    please work

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,17 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     appType: 'mpa',
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                password: 'password/index.html',
+                qr: 'qr/index.html',
+                jpg: 'jpg/index.html',
+                bpm: 'bpm/index.html',
+                sqrt: 'sqrt/index.html',
+                mp4: 'mp4/index.html',
+            }
+        }
+    }
 });
